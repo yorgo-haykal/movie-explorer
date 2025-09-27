d79ff1cb5a10c6ba7cafd047375b1a21df633a2e
Yorgo Haykal <yorgohaykal004@gmail.com>
2025-09-27 17:03:57 +0200

    Added the api calls and initial movie search

--- movie-explorer/src/components/SearchBar.jsx
+++ movie-explorer/src/components/SearchBar.jsx
@@ -1,10 +1,23 @@
+import { useState } from 'react';
 import './SearchBar.css'
 
-export default function SearchBar() {
+export default function SearchBar({onSearch}) {
+  const [query, setQuery] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (query.trim()) {
+      onSearch(query.trim());
+    }
+  }
+
   return (
-    <div>
-      <input className="search-bar-input" type="text"></input>
-      <button id='search-button'>Search</button>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <input className="search-bar-input" 
+        type="text" value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <button id='search-button' type='submit'>Search</button>
+    </form>
   );
 }
